253f5d460dd865edbd1cca465b3d201b693b7466
可能不是鱼的非鱼罐头 <382479637@qq.com>
2026-07-17 15:35:22 +0800

    修复了存在前端后端单一启动器的问题

--- start-backend.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0backend"
-call venv\Scripts\activate.bat
-echo 启动后端服务...
-echo API 文档: http://localhost:8080/docs
-echo.
-uvicorn main:app --reload --host 0.0.0.0 --port 8080
-pause
--- start-frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0frontend"
-echo 启动前端服务...
-echo 页面地址: http://localhost:5173
-echo.
-call npm run dev
-pause
